a140b880eb057f6b817201127e57ea0b520ce225
tanviranik004 <tanvir.anik.009@gmail.com>
2020-09-13 14:14:52 +0600

    first commit

---
+++ src/components/Friend/Friend.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Friend = (props) => {
+    const {name, email} = props.friend;
+    const friendStyle = {
+        border: '1px solid purple',
+        margin: '20px',
+        paddings: '20px',
+        borderRadius: '20px'
+
+    }
+    return (
+        <div style={friendStyle}>
+            <h2>Name: {name}</h2>
+            <p>email:{email}</p>
+        </div>
+    );
+};
+
+export default Friend;
