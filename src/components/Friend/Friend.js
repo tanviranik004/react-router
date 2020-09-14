d70562b2dfd02428b849d08dc84975382dc151b6
tanviranik004 <tanvir.anik.009@gmail.com>
2020-09-14 13:46:03 +0600

    final commit

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
-    const {name, email} = props.friend;
+    const {name, email,id} = props.friend;
+    const history = useHistory();
+    const handleClick =(friendId)=>{
+        const url = `/friend/${friendId}`;
+        history.push(url);
+    }
     const friendStyle = {
         border: '1px solid purple',
         margin: '20px',
@@ -9,10 +15,21 @@ const Friend = (props) => {
         borderRadius: '20px'
 
     }
+    const buttonStyle = {
+        backgroundColor:'#4CAF50',
+        border: '1px solid tomato',
+        margin: '5px',
+        display: 'inline-block',
+        fontSize: '16px',
+        paddings: '15px 32px',
+        borderRadius: '5px'
+    }
     return (
         <div style={friendStyle}>
             <h2>Name: {name}</h2>
             <p>email:{email}</p>
+            <Link to={`/friend/${id}`}>Show detail of id:{id}</Link>
+           <button style={buttonStyle} onClick={()=>handleClick(id)}>Click me</button>
         </div>
     );
 };
